5dd6fd98be8d231fd9ec3dd45f52ed24d476825a
Maria Edlinger <medlinger.mmt-b2018@fh-salzburg.ac.at>
2019-10-29 23:26:13 +0100

    burger menu and responsive footer

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 import style from './styles.scss';
+
+
+function displayMenu() {
+    document.getElementById("menu").style.display = "block";
+}
+
+function hideMenu() {
+    document.getElementById("menu").style.display = "none";
+}
+
+document.getElementById("burger").addEventListener("click", displayMenu);
+document.getElementById("hide").addEventListener("click", hideMenu); 
+
+// window.addEventListener('resize', displayMenu);
