4e972d0661d8aaa5c7b76b00f5e9d91b735b1f87
Maria Edlinger <medlinger.mmt-b2018@fh-salzburg.ac.at>
2019-11-04 21:16:29 +0100

    changed button color and delete lines in js-file

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
 import style from './styles.scss';
-
-
-function displayMenu() {
-    document.getElementById("menu").style.display = "block";
-}
-
-function hideMenu() {
-    document.getElementById("menu").style.display = "none";
-}
-
-document.getElementById("burger").addEventListener("click", displayMenu);
-document.getElementById("hide").addEventListener("click", hideMenu); 
